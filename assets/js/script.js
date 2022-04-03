c8938708e76d3a285c0e79e2fa22cb4e46b1421f
Derricka S <derrickas728@gmail.com>
2022-04-03 17:39:10 -0400

    add questions in an array

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var qAndA = [{
+    question : "Commonly used data types do NOT include:",
+    answers : ["strings", "booleans", "alerts", "numbers"],
+    tAnswer :  "alerts"
+}, {
+    question : "The condition in an if/else statement is enclosed with __________",
+    answers : ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    tAnswer :  "parenthesis"
+}, {
+    question : "Arrays in JavaScript can be used to store ____________",
+    answers : ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    tAnswer :  "all of the above"
+}, {
+    question : "Strings values must be enclosed within ___________ when being assigned variables",
+    answers : ["commas", "curly brackets", "quotes", "parenthesis"],
+    tAnswer :  "quotes"
+}, {
+    question : "A very useful tool used during development and debugging for printing content to the debugger is",
+    answers : ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    tAnswer :  "curly brackets"
+}];
