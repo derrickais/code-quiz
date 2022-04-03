0c4d8a20f32be07943e2feff7e742bdd077fac99
Derricka S <derrickas728@gmail.com>
2022-04-03 18:16:51 -0400

    add checkAnswer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var mainEl = document.querySelector("main");
+var buttonEl = document.querySelector("button");
+
 var qAndA = [{
     question : "Commonly used data types do NOT include:",
     answers : ["strings", "booleans", "alerts", "numbers"],
@@ -19,3 +22,40 @@ var qAndA = [{
     answers : ["JavaScript", "terminal/bash", "for loops", "console.log"],
     tAnswer :  "curly brackets"
 }];
+
+var startQuiz = function(event){
+    //changes h1 into question
+    var h1El = document.querySelector("h1");
+    h1El.textContent = qAndA[1].question;
+    
+    //removes <p>
+    var pEl = document.querySelector("p");
+    pEl.remove();
+    
+    //creates <li> for each answer
+    var ulEl = document.querySelector("ul");
+    for(i = 0; i < 4; i++){
+        var liEl = document.createElement("li");
+        liEl.textContent = qAndA[1].answers[i];
+        ulEl.appendChild(liEl);
+    }
+
+    //remove start quiz button
+    buttonEl.remove()
+
+    ulEl.addEventListener("click", checkAnswer);
+}
+
+var checkAnswer = function(event){
+    console.log(event.target.textContent);
+    if (event.target.textContent == qAndA[1].tAnswer){
+        alert("woo");
+    }else{
+        alert("boo");
+    }
+}
+
+
+
+buttonEl.addEventListener("click", startQuiz);
+
